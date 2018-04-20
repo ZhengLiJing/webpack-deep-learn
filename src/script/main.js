7d5cf17ac10b4d6f0907c94fcc98313f434b9a09
jingke <987906521@qq.com>
2018-04-20 16:07:14 +0800

    html-webpack-plugin

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,5 @@
-function add() {}
+function add() {
+    alert('nihao');
+}
+
+add();
--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,45 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     entry: {
         a: './src/script/a.js',
-        main: './src/script/main.js'
+        main: './src/script/main.js',
+        b: './src/script/b.js',
+        c: './src/script/c.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name]-[chunkhash].js'
+        filename: 'js/[name]-[chunkhash].js',
+        publicPath: 'http://cdn.com'
     },
     plugins: [
         new htmlWebpackPlugin({
             template: 'index.html',
-            inject: 'body',
-            title: 'webpack is very nice'
+            inject: false,
+            title: 'this is a.html',
+            filename: 'a.html',
+            excludeChunks: ['b', 'c'],
+            minify: {
+                collapseWhitespace: true
+            }
+        }),
+        new htmlWebpackPlugin({
+            template: 'index.html',
+            inject: false,
+            title: 'this is b.html',
+            filename: 'b.html',
+            excludeChunks: ['a', 'c'],
+            minify: {
+                collapseWhitespace: true
+            }             
+        }),
+        new htmlWebpackPlugin({
+            template: 'index.html',
+            inject: false,
+            title: 'this is c.html',
+            filename: 'c.html',
+            excludeChunks: ['a', 'b'],
+            minify: {
+                collapseWhitespace: true
+            }     
         }),
         new CleanWebpackPlugin(path.resolve(__dirname, 'dist')),
     ]
